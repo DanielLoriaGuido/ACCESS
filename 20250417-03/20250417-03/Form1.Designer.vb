3fe02b9d91c29aab8b2d68bd34d05134840dbc02
Loria Guido　Daniel Alonso <st002490@m03.kyoto-kcg.ac.jp>
2025-04-17 12:38:18 +0900

    Use of KeyChar

---
+++ 20250417-03/20250417-03/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.labkekka = New System.Windows.Forms.Label()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pic2
+        '
+        Me.pic2.Image = CType(resources.GetObject("pic2.Image"), System.Drawing.Image)
+        Me.pic2.Location = New System.Drawing.Point(251, 259)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(301, 233)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic2.TabIndex = 1
+        Me.pic2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(235, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "年齢"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(235, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "身長"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(342, 141)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pic1
+        '
+        Me.pic1.Image = CType(resources.GetObject("pic1.Image"), System.Drawing.Image)
+        Me.pic1.Location = New System.Drawing.Point(251, 259)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(301, 234)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 7
+        Me.pic1.TabStop = False
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(313, 49)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(159, 19)
+        Me.txt1.TabIndex = 8
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(313, 98)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(159, 19)
+        Me.txt2.TabIndex = 9
+        '
+        'labkekka
+        '
+        Me.labkekka.AutoSize = True
+        Me.labkekka.Location = New System.Drawing.Point(371, 208)
+        Me.labkekka.Name = "labkekka"
+        Me.labkekka.Size = New System.Drawing.Size(38, 12)
+        Me.labkekka.TabIndex = 10
+        Me.labkekka.Text = "Result"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 490)
+        Me.Controls.Add(Me.labkekka)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.pic1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pic2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pic2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents labkekka As Label
+End Class
---
+++ 20250417-03/20250417-03/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nnennrei As Integer
+        Dim nshinchou As Integer
+        Dim result As String
+
+        nnennrei = Me.txt1.Text
+        nshinchou = Me.txt2.Text
+
+        If nnennrei >= 10 Or nshinchou >= 110 Then
+            result = "OK"
+            pic1.Visible = False
+            pic2.Visible = True
+        Else
+            result = "残念ですが次は乗りましょう"
+            pic1.Visible = True
+            pic2.Visible = False
+        End If
+
+        Me.labkekka.Text = result
+    End Sub
+    Private Sub txt1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt1.KeyPress
+        'The keypressed method uses the KeyChar property to check whether the ENTER key pressed.
+        'If the ENTER key is pressed, the Handled property is set to true, which indicates the event is handled.
+        Select Case e.KeyChar
+            Case "0"c To "9"c 'Determina los caracteres a utilizar
+            Case ChrW(8) 'Permite la accion de borrado
+            Case Else
+                e.Handled = True ' No permite otros caracteres
+        End Select
+    End Sub
+
+    Private Sub txt2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt2.KeyPress
+        Select Case e.KeyChar
+            Case "0"c To "9"c
+            Case ChrW(8)
+            Case Else
+                e.Handled = True
+        End Select
+    End Sub
+End Class
